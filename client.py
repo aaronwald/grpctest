8ee70cbba6a2f7e1457a626912165d921a3406fb
Aaron Wald <aaronwald@gmail.com>
2024-11-25 17:19:55 -0500

    fix

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import helloworld_pb2_grpc
 
 
 def run():
-    with grpc.insecure_channel('localhost:8081/hello') as channel:
+    with grpc.insecure_channel('localhost:8081/') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='World'))
         print("Greeter client received: " + response.message)
